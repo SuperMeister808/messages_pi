9e81ad23d949e0365fa8a8144101b799af1b75fc
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-17 11:01:18 +0100

    test_write_on_database passed

--- tests/test_write_on_database.py
+++ tests/test_write_on_database.py
@@ -2,6 +2,32 @@
 import unittest
 from unittest.mock import patch
 
-class TestWriteOnDatabase():
+from server import Server
+from database import Database
 
-    
+class TestWriteOnDatabase(unittest.TestCase):
+
+    def setUp(self):
+
+        self.server = Server("Testing...", "Testing...")
+        self.test_client = self.server.app.test_client()
+
+    def test_call_write_on_database(self):
+
+        data = {"titel": "titel", "message": "message"}
+        
+        with patch.object(Database, "connect_to_database") as connect:
+
+            with patch.object(Database, "add_message") as add:
+
+                response = self.test_client.post("/send", json=data)  
+
+                connect.assert_called_once()
+                add.assert_called_once()
+
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.json, {"Success": "Thanks for you request!"})     
+
+if __name__ == "__main__":
+
+    unittest.main()
