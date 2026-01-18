fc4f43aa560864ec5c93c99748b81b61d144d7b7
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-18 15:23:16 +0100

    Refactor Database

--- database.py
+++ database.py
@@ -3,53 +3,41 @@ import sqlite3
 
 class Database():
 
-    def __init__(self):
-
-        self.conn = None
-
-        self.cursor = None
-    
-    def connect_to_database(self):
-
-        self.conn = sqlite3.connect("messages.db")
-
-        self.cursor = self.conn.cursor()
-
     def create_table(self):
-
-        self.connect_to_database()
         
-        self.cursor.execute("""
+        conn = sqlite3.connect("messages.db")
+        cursor = conn.cursor()
+        
+        cursor.execute("""
 CREATE TABLE IF NOT EXISTS messages(
     titel TEXT,
     message TEXT)""")
-        self.conn.commit()
+        conn.commit()
 
-        self.close_connection()
+        conn.close()
         
     def add_message(self, titel, message):
 
-        self.connect_to_database()
+        conn = sqlite3.connect("messages.db")
+        cursor = conn.cursor()
 
-        self.cursor.execute("""
+        cursor.execute("""
 INSERT INTO messages (titel, message) VALUES (?, ?)""", (titel, message))    
-        self.conn.commit()
+        conn.commit()
 
-        self.close_connection()
+        conn.close()
 
     def print_table(self):
 
-        self.connect_to_database()
+        conn = sqlite3.connect("messages.db")
+        cursor = conn.cursor()
         
-        self.cursor.execute("SELECT * FROM messages")
+        cursor.execute("SELECT * FROM messages")
         colums = [desc[0] for desc in self.cursor.description]
         rows = self.cursor.fetchall()
 
         print(colums)
         print(rows)
 
-        self.close_connection()
+        conn.close()
 
-    def close_connection(self):
-
-        self.conn.close()
--- tests/test_write_on_database.py
+++ tests/test_write_on_database.py
@@ -5,6 +5,8 @@ from unittest.mock import patch
 from server import Server
 from database import Database
 
+import sqlite3
+
 class TestWriteOnDatabase(unittest.TestCase):
 
     def setUp(self):
@@ -16,7 +18,7 @@ class TestWriteOnDatabase(unittest.TestCase):
 
         data = {"titel": "titel", "message": "message"}
         
-        with patch.object(Database, "connect_to_database"):
+        with patch.object(Database, "create_table"):
 
             with patch.object(Database, "add_message") as add:
 
@@ -26,6 +28,32 @@ class TestWriteOnDatabase(unittest.TestCase):
 
                 self.assertEqual(response.status_code, 200)
                 self.assertEqual(response.json, {"Success": "Thanks for you request!"})     
+
+    def test_write_on_database_correct_request(self):
+
+        data = {"titel": "Test", "message": "This is a test"}
+
+        original_connect = sqlite3.connect
+        
+        with patch("sqlite3.connect", side_effect=lambda _:original_connect(":memory:")) as connect:
+
+            response = self.test_client.post("/send", json=data)
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json, {"Success": "Thanks for you request!"})
+
+            conn = sqlite3.connect("Testing...")
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM messages")
+            colums = [desc[0] for desc in cursor.description]
+            rows = cursor.fetchall()
+
+            self.assertEqual(colums, ['titel', 'message'])
+            self.assertEqual(rows, [("Test", "This is a test")])
+
+            conn.close()
+
 
 if __name__ == "__main__":
 
