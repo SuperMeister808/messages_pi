1eb9bbcd17577071158c38eab97fa6e29d766202
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-18 15:38:43 +0100

    One Connection to add a message

--- database.py
+++ database.py
@@ -3,28 +3,33 @@ import sqlite3
 
 class Database():
 
-    def create_table(self):
+    def create_table(self, conn):
         
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages(
+            titel TEXT,
+            message TEXT)""")
+        
+        conn.commit()
+        
+    def add_message(self, conn, titel, message):
+
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        INSERT INTO messages (titel, message) VALUES (?, ?)""", (titel, message))    
+        
+        conn.commit()
+
+    def write_on_database(self, titel, message):
+
         with sqlite3.connect("messages.db") as conn:
-            cursor = conn.cursor()
-        
-            cursor.execute("""
-            CREATE TABLE IF NOT EXISTS messages(
-                titel TEXT,
-                message TEXT)""")
-            conn.commit()
-        
-    def add_message(self, titel, message):
 
-        self.create_table()
-        
-        with sqlite3.connect("messages.db") as conn:
-            cursor = conn.cursor()
-
-            cursor.execute("""
-            INSERT INTO messages (titel, message) VALUES (?, ?)""", (titel, message))    
-            conn.commit()
-
+            self.create_table(conn)
+            self.add_message(conn, titel, message)
+    
     def print_table(self):
 
         with sqlite3.connect("messages.db") as conn:
