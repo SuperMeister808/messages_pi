12814f05b143a614068edaf3c0af741a62778461
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-22 15:48:26 +0100

    patched handlers in test_extract_data

--- tests/test_extract_data.py
+++ tests/test_extract_data.py
@@ -3,6 +3,8 @@ import unittest
 from unittest.mock import patch
 
 from server import Server
+from message_handler import MessageHandler
+from message_reader import MessageReader
 
 from message import CollectMessage
 from title import CollectTitle
@@ -11,8 +13,10 @@ class TestExtractData(unittest.TestCase):
 
     def setUp(self):
         
-        self.server = Server("Testing...", "Testing...")
-
+        self.message_handler = MessageHandler()
+        self.message_reader = MessageReader()
+        
+        self.server = Server("Testing...", "Testing...", self.message_handler, self.message_reader)
         self.test_client = self.server.app.test_client()
 
     def test_correct_json_format(self):
@@ -20,8 +24,8 @@ class TestExtractData(unittest.TestCase):
         data = {"title": "title", "message": "message"}
 
         with patch("sqlite3.connect", return_value="not relevant") as connection:
-            with patch.object(Server, "clear_data") as clear:
-                with patch.object(Server, "write_data") as database:
+            with patch.object(MessageHandler, "clear_data") as clear:
+                with patch.object(MessageHandler, "write_data") as database:   
                         with patch.object(Server, "close_connection") as close:
 
                             response = self.test_client.post("/send", json=data)
@@ -49,8 +53,8 @@ class TestExtractData(unittest.TestCase):
 
         with patch("sqlite3.connect", return_value="not relevant") as connection:
             with patch.object(Server, "close_connection") as close:
-                with patch.object(Server, "clear_data") as clear:
-                    with patch.object(Server, "write_data") as write:
+                with patch.object(MessageHandler, "clear_data") as clear:
+                    with patch.object(MessageHandler, "write_data") as write:
             
                         response = self.test_client.post("/send", json=data)
             
@@ -75,8 +79,8 @@ class TestExtractData(unittest.TestCase):
 
         with patch("sqlite3.connect", return_value="not relevant") as connection:
             with patch.object(Server, "close_connection") as close:
-                with patch.object(Server, "clear_data") as clear:
-                    with patch.object(Server, "write_data") as write:
+                with patch.object(MessageHandler, "clear_data") as clear:
+                    with patch.object(MessageHandler, "write_data") as write:
                 
                         response = self.test_client.post("/send", json=data)
             
@@ -98,8 +102,8 @@ class TestExtractData(unittest.TestCase):
 
         data = {"title": "title", "message": "message", "extra": "extra"}
 
-        with patch.object(Server, "clear_data") as clear:
-            with patch.object(Server, "write_data") as write:
+        with patch.object(MessageHandler, "clear_data") as clear:
+            with patch.object(MessageHandler, "write_data") as write:
                 with patch("sqlite3.connect", return_value="not relevant") as connection:
                     with patch.object(Server, "close_connection") as close:
 
